f232c4d79bd113ca8d83393e44271fecd6bdd841
Кирилл Пименов <kirill@pimenov.cc>
2012-03-10 04:17:49 +0400

    Пробел переключает состояние

--- life.erl
+++ life.erl
@@ -8,48 +8,75 @@ run()->
   cecho:cbreak(),
   cecho:noecho(),
 
-  paint_life(generate()),
+  cecho:curs_set(0),
 
-  event_loop().
+  FIELD = generate(),
+  event_loop(FIELD).
 
 generate()->[
-    [0,0,0,0,0,0,0],
     [0,0,1,0,0,0,0],
     [0,0,1,0,0,0,0],
     [0,0,1,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,1,0],
-    [0,0,0,0,0,0,0]
+    [0,0,1,0,0,0,0],
+    [0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0],
+    [0,0,1,0,0,0,0]
   ].
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-event_loop()->
+event_loop(FIELD)->
+  paint_life(FIELD),
+  cecho:refresh(),
+  cecho:move(0,0),
+
   C = cecho:getch(),
   case C of
     $q ->
-      %% If we get a 'q' then exit the mover and stop cecho
+      %% If we get a 'q' then exit
       application:stop(cecho),
       erlang:halt();
+    $  ->
+      %% If we get a ' ' then iterate
+      NEW_FIELD = iterate_field(FIELD),
+      event_loop(NEW_FIELD);
     _ ->
       %% ignore anything else
-      event_loop()
+      event_loop(FIELD)
     end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+iterate_field(FIELD)->
+  toggle(FIELD).
+
+toggle([])->
+  [];
+toggle([ROW|OTHER])->
+ [toggle_row(ROW) | toggle(OTHER)].
+
+toggle_row([])->
+  [];
+toggle_row([A|OTHER])->
+ [1-A | toggle_row(OTHER)].
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-paint_life([])->
+paint_life(FIELD)->
+  paint_life(FIELD, 0).
+
+paint_life([],_X)->
   ok;
-paint_life([ROW|OTHER])->
-  paint_row(ROW),
+paint_life([ROW|OTHER], X)->
+  paint_row(ROW, X, 0),
   io:format("~n"),
-  paint_life(OTHER).
+  paint_life(OTHER, X+1).
 
-paint_row([])->
+paint_row([], _X, _Y)->
   ok;
-paint_row([CELL|OTHER])->
-  paint_cell(CELL),
-  paint_row(OTHER).
+paint_row([CELL|OTHER], X, Y)->
+  paint_cell(CELL, X, Y),
+  paint_row(OTHER, X, Y+1).
 
-paint_cell(1)->
-  io:put_chars("#");
-paint_cell(_)->
-  io:put_chars(".").
+paint_cell(1, X, Y)->
+  cecho:mvaddstr(X, Y, "#");
+paint_cell(_, X, Y)->
+  cecho:mvaddstr(X, Y, ".").
